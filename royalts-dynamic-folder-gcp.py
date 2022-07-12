efe212446583abd33fcc99adce9aeabd80f8f4ca
Petri Saarilammi <481317+Jeremiad@users.noreply.github.com>
2022-07-12 23:36:14 +0300

    fixes and debugging added

--- royalts-dynamic-folder-gcp.py
+++ royalts-dynamic-folder-gcp.py
@@ -1,43 +1,52 @@
-import json, subprocess, sys, logging
+from asyncio.log import logger
+import json, subprocess, sys, logging, re
 
 def get_instances(project, account = "", gateway = ""):
-    cmd = "gcloud compute instances list --format json --project " + project
+    cmd = "gcloud compute instances list --format json --project {}".format(project)
+    #logger.info("Account: {}".format(account))
 
-    if account != "$CustomProperty.Account$":
-        if account != "":
-            cmd += " --account " + account
+    if re.match("^\$CustomProperty\.Account\$", account) == None:
+        cmd += " --account {}".format(account)
+    #logger.info("Command: {}".format(cmd))    
 
-    gcp = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
-    (response_json, err) = gcp.communicate()
-    exit_code = gcp.wait()
+    try:
+        gcp = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+        (response_json, err) = gcp.communicate()
+        exit_code = gcp.wait()
+        #logger.info("Process exit code: {}".format(exit_code))
+        #logger.info("Raw Response: {}".format(response_json))
 
-    response = json.loads(response_json)
+        if err != None:
+            logger.error(err)
 
-    connections = []
+        response = json.loads(response_json)
+        connections = []
 
-    for instance in response:
-        connection = {}
-        public_ip_address = ""
-        private_ip_address = ""
-        for nics in instance.get("networkInterfaces"):
-            if nics.get("accessConfigs") != None:
-                for extnic in nics.get("accessConfigs"):
-                    if extnic.get("natIP") != None:
-                        public_ip_address = extnic.get("natIP")
-            private_ip_address = nics.get("networkIP")
-        connection["Name"] = instance.get("name")
-        if public_ip_address != "":
-            connection["ComputerName"] = public_ip_address
-        else:
-            connection["ComputerName"] = private_ip_address
-            if gateway != "$CustomProperty.Gateway$":
-                if gateway != "":
-                    connection["SecureGatewayID"] = gateway
-        connection["Type"] = "TerminalConnection"
-        connection["TerminalConnectionType"] = "SSH"
-        connection["CredentialsFromParent"] = "true"
-        #connection["SecureGatewayFromParent"] = "true"
-        connections.append(connection)
+        for instance in response:
+            connection = {}
+            public_ip_address = ""
+            private_ip_address = ""
+            for nics in instance.get("networkInterfaces"):
+                if nics.get("accessConfigs") != None:
+                    for extnic in nics.get("accessConfigs"):
+                        if extnic.get("natIP") != None:
+                            public_ip_address = extnic.get("natIP")
+                private_ip_address = nics.get("networkIP")
+            connection["Name"] = instance.get("name")
+            if public_ip_address != "":
+                connection["ComputerName"] = public_ip_address
+            else:
+                connection["ComputerName"] = private_ip_address
+                if gateway != "$CustomProperty.Gateway$":
+                    if gateway != "":
+                        connection["SecureGatewayID"] = gateway
+            connection["Type"] = "TerminalConnection"
+            connection["TerminalConnectionType"] = "SSH"
+            connection["CredentialsFromParent"] = "true"
+            #connection["SecureGatewayFromParent"] = "true"
+            connections.append(connection)
+    except Exception as ex:
+        logging.error(ex)
 
     store = {
         "Objects": connections
@@ -49,6 +58,7 @@ def royalts():
     project = "$CustomProperty.Project$"
     account = "$CustomProperty.Account$"
     gateway = "$CustomProperty.Gateway$"
+
     print(get_instances(project, account, gateway))
 
 def development():
@@ -67,5 +77,5 @@ def main():
     #development()
 
 if __name__ == '__main__':
-    logging.basicConfig(filename='debug.log', level=logging.DEBUG)
+    logging.basicConfig(filename='royalts-dynamic-folder-gcp-debug.log', level=logging.DEBUG, format='[%(asctime)s] Level:[%(levelname)s] [Function:%(funcName)s] %(message)s')
     main()
